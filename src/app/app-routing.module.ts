749e2545e60377dcc44906a2f59d2a0b81c9a09e
Nikesh <nikesh9220@gmail.com>
2020-09-04 17:40:15 +0530

    Authentication and Authguard Added.

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,14 +1,25 @@
 import { NgModule } from '@angular/core';
-import { Routes, RouterModule } from '@angular/router';
+import { Routes, RouterModule, Route } from '@angular/router';
+import { AuthGuard } from './services/auth.guard';
+const indexRoute: Route = {
+  path: '',
+  loadChildren: './login/login.module#LoginModule',
+  data: { showHeader: false, showSidebar: false, showFooter: false }
+};
 
+const fallbackRoute: Route = {
+  path: '**',
+  loadChildren: './login/login.module#LoginModule',
+  data: { showHeader: false, showSidebar: false, showFooter: false }
+};
 const routes: Routes = [
-  {
-    path: '',
-    redirectTo: '/login',
-    pathMatch: 'full'
-  },
-  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule' },
-  { path: 'users', loadChildren: './users/users.module#UsersModule' },
+  // {
+  //   path: '',
+  //   redirectTo: '/dashboard',
+  //   pathMatch: 'full',
+  // },
+  { path: 'dashboard', loadChildren: './dashboard/dashboard.module#DashboardModule',canActivate:[AuthGuard] },
+  { path: 'users', loadChildren: './users/users.module#UsersModule'},
   {
     path: 'login',
     loadChildren: './login/login.module#LoginModule',
@@ -18,7 +29,10 @@ const routes: Routes = [
     path: 'registration',
     loadChildren: './registration/registration.module#RegistrationModule',
     data: { showHeader: false, showSidebar: false, showFooter: false }
-  }
+  },
+  fallbackRoute,
+  indexRoute
+  
 ];
 
 @NgModule({
--- src/app/dashboard/dashboard-routing.module.ts
+++ src/app/dashboard/dashboard-routing.module.ts
@@ -1,9 +1,9 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { DashboardComponent } from './dashboard.component';
-
+import { AuthGuard } from '../services/auth.guard'
 const routes: Routes = [
-  { path: '', component: DashboardComponent }
+  { path: '',component: DashboardComponent},
 ];
 
 @NgModule({
